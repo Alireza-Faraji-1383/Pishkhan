644205bcb600abc40e7b9698cdd5341e98d9f45b
Alireza-Faraji-1383 <farajialireza664@gmail.com>
2025-10-21 17:23:55 +0330

    add version

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Hotel , RoomType
+from .models import Hotel , RoomType ,Reservation
 
 admin.site.register(Hotel)
 admin.site.register(RoomType)
+admin.site.register(Reservation)
