69e9c7170d0269cedcbe3fbaf8a42f293a697386
Alireza-Faraji-1383 <farajialireza664@gmail.com>
2026-06-22 21:42:40 +0400

    feat: register Review model in Django admin with improved admin configs

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
-from .models import Hotel , RoomType ,Reservation
+from .models import Hotel, RoomType, Reservation, Review
 
-admin.site.register(Hotel)
-admin.site.register(RoomType)
-admin.site.register(Reservation)
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'owner')
+    search_fields = ('name', 'city', 'address')
+    list_filter = ('city',)
+
+
+@admin.register(RoomType)
+class RoomTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'hotel', 'price_per_night', 'capacity', 'inventory')
+    list_filter = ('hotel',)
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room_type', 'check_in_date', 'check_out_date', 'status')
+    list_filter = ('status',)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'hotel', 'rating', 'created_at')
+    list_filter = ('rating', 'hotel')
+    search_fields = ('comment',)
